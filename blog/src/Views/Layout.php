78ba9c417340911c7e892bde4617a94c6808cd49
Ronnie B <trafficinc@users.noreply.github.com>
2020-11-28 13:56:16 -0500

    Update Layout.php

--- blog/src/Views/Layout.php
+++ blog/src/Views/Layout.php
@@ -21,7 +21,7 @@ class Layout
     {
         ?>
         <!DOCTYPE html>
-        <html xmlns="http://www.w3.org/1999/xhtml">
+        <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
